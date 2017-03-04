77cb4808f2c05e5c43b91ca1d6279458035f08e7
jeonghun Ji <jj.jeonghun.ji@gmail.com>
2017-03-04 17:27:56 +0900

    .

--- src/main/java/bitcamp/java89/ems2/servlet/HeaderServlet.java
+++ src/main/java/bitcamp/java89/ems2/servlet/HeaderServlet.java
@@ -2,12 +2,19 @@ package bitcamp.java89.ems2.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import bitcamp.java89.ems2.domain.Manager;
+import bitcamp.java89.ems2.domain.Member;
+import bitcamp.java89.ems2.domain.Photo;
+import bitcamp.java89.ems2.domain.Student;
+import bitcamp.java89.ems2.domain.Teacher;
 
 @WebServlet("/header")
 public class HeaderServlet extends HttpServlet {
@@ -27,5 +34,34 @@ public class HeaderServlet extends HttpServlet {
         + " margin-left:60px; padding-top:7px; font-family:돋움체,sans-serif;"
         + " font-size:x-large;'>교육센터관리시스템</div>");
     out.println("</div>");
+    
+    // 로그인 사용자 정보를 가져온다.
+    out.println("<div style='height:30px; float:right; margin:5px;'>");
+    Member member = (Member)request.getSession().getAttribute("member");
+    if (member == null) {
+      out.println("<a href='../auth/login'>로그인</a>");
+    } else {
+      out.printf("<img src='../upload/%s' height='30' style='vertical-align:middle;'>\n", this.getPhotoPath(member));
+      out.printf("<span>%s</span>\n", member.getName());
+      out.println("<a href='../auth/logout'>로그아웃</a>");
+    }
+    out.println("</div>");
+    
+    out.println("</div>");
   }  
+  private String getPhotoPath(Member member) {
+    if (member instanceof Student) {
+      return ((Student)member).getPhotoPath();
+    } else if (member instanceof Manager) {
+      return ((Manager)member).getPhotoPath();
+      
+    } else /*if (member instanceof Teacher)*/ {
+      List<Photo> photoList = ((Teacher)member).getPhotoList();
+      if (photoList.size() > 0) {
+        return photoList.get(0).getFilePath();
+      } else {
+        return null;
+      }
+    }
+  }
 }
